ec9eaee1d9af195bb88cb52b0f0cd4020e640012
DanielSitoV01D <daniel.cabrera.lazo@gmail.com>
2025-06-18 00:28:43 -0500

    Cambios a classifier.py

--- app/classifier.py
+++ app/classifier.py
@@ -8,6 +8,9 @@ BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 # Ruta absoluta al modelo
 ruta_modelo = os.path.join(BASE_DIR, "models", "modelo_clasificador_trafico_red.pkl")
 
+#Cargar modelo
+modelo = joblib.load(ruta_modelo)
+
 # Definir el orden exacto de las variables que el modelo espera
 columnas_modelo = [
     "Flow Duration", "Total Fwd Packets", "Total Backward Packets",
